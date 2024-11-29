cc8723348a36ea5aa7a5a1c6b969cd2781af0b09
RamanaS25 <42925523+RamanaS25@users.noreply.github.com>
2024-11-29 21:15:35 +0530

    env updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://tdyffipvsufcsiamzhwo.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkeWZmaXB2c3VmY3NpYW16aHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI4MDE1MTIsImV4cCI6MjAxODM3NzUxMn0.hNVixCvQ4RYVRXn7ZIsVrRxVVn_QIvzQloJcWwgGkE0',
 };
